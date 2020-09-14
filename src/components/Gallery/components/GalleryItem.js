6fabfdbeb3bc94fb9799e10a4f34a298003964de
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-14 20:59:43 +0200

    Formatting with Prettier

--- src/components/Gallery/components/GalleryItem.js
+++ src/components/Gallery/components/GalleryItem.js
@@ -1,26 +1,34 @@
 import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 
-const GalleryItem = ({id, source, thumbnail, caption, description, position, toggleLightbox}) => {
+const GalleryItem = ({
+  id,
+  source,
+  thumbnail,
+  caption,
+  description,
+  position,
+  toggleLightbox,
+}) => {
+  const onClick = useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleLightbox(position)
+    },
+    [position, toggleLightbox]
+  )
 
-    const onClick = useCallback((e) => {
-        e.preventDefault()
-        toggleLightbox(position)
-    }, [position, toggleLightbox]);
-
-    return (<article key={id} className="6u 12u$(xsmall) work-item">
-        <a
-        className="image fit thumb"
-        href={source}
-        onClick={onClick}
-        >
+  return (
+    <article key={id} className="6u 12u$(xsmall) work-item">
+      <a className="image fit thumb" href={source} onClick={onClick}>
         <img src={thumbnail} />
-        </a>
+      </a>
 
-        <h3>{caption}</h3>
-        <p>{description}</p>
-    </article>)
-};
+      <h3>{caption}</h3>
+      <p>{description}</p>
+    </article>
+  )
+}
 
 GalleryItem.displayName = 'GalleryItem'
 GalleryItem.propTypes = {
@@ -30,7 +38,7 @@ GalleryItem.propTypes = {
   caption: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   position: PropTypes.string.isRequired,
-  toggleLightbox: PropTypes.func.isRequired
+  toggleLightbox: PropTypes.func.isRequired,
 }
 
 export default GalleryItem
