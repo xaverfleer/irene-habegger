50fd5483a5277b65793729d34776a94414d519bd
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-14 21:16:12 +0200

    Fix href's

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,22 @@ const Footer = () => (
     <div className="inner">
       <ul className="icons">
         <li>
-          <a href="#" className="icon fa-twitter">
+          <a href="/#" className="icon fa-twitter">
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-github">
+          <a href="/#" className="icon fa-github">
             <span className="label">Github</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-dribbble">
+          <a href="/#" className="icon fa-dribbble">
             <span className="label">Dribbble</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-envelope-o">
+          <a href="/#" className="icon fa-envelope-o">
             <span className="label">Email</span>
           </a>
         </li>
