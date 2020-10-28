a95b836663d682f51e7b3ed9ccdceaa9bc936736
Xaver Fleer <xaver.fleer@gmail.com>
2020-10-28 09:52:22 +0100

    Fix overlay image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
+import overlay from '../assets/scss/images/overlay.png'
 
 const Header = ({ bgImage, meImage }) => (
   <header
     id="header"
-    style={{ backgroundImage: `url('images/overlay.png'), url(${bgImage})` }}
+    style={{ backgroundImage: `url(${overlay}), url(${bgImage})` }}
   >
     <div className="inner">
       <a href="/#" className="image avatar">
