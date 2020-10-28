5d5d4e0a35efdfff66812743b5a93ea672931ac1
Xaver Fleer <xaver.fleer@gmail.com>
2020-10-28 12:09:05 +0100

    Format with Prettier

--- src/netlify-functions/private/mailing.js
+++ src/netlify-functions/private/mailing.js
@@ -1,14 +1,14 @@
-const sgMail = require("@sendgrid/mail");
+const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = {
   sendEmail(message) {
     const messageWithSender = {
       ...message,
-      bcc: "xaver.fleer+soko-copy@gmail.com",
-      from: "kurs@so-kommunizieren.ch",
-    };
-    return sgMail.send(messageWithSender);
+      bcc: 'xaver.fleer+soko-copy@gmail.com',
+      from: 'kurs@so-kommunizieren.ch',
+    }
+    return sgMail.send(messageWithSender)
   },
-};
+}
