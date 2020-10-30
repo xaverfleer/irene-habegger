1b253c78895d72ae36ee69bc439eb7aecc506068
Xaver Fleer <xaver.fleer@gmail.com>
2020-10-30 11:17:50 +0100

    Connect form frontend to backend

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ContactForm = () => (
-  <form method="post" action="#">
+  <form onSubmit={handleSubmit}>
     <div className="row uniform 50%">
       <div className="6u 12u$(xsmall)">
         <input
@@ -33,10 +33,58 @@ const ContactForm = () => (
     </div>
     <ul className="actions" style={{ marginTop: 30 }}>
       <li>
-        <input type="submit" value="Absenden" />
+        <input name="submit" type="submit" value="Absenden" />
       </li>
     </ul>
   </form>
 )
+
+function getFormData(formElem) {
+  return [].slice
+    .call(formElem, 0)
+    .map(({ name, value }) => ({ name, value }))
+    .filter(({ name }) => name !== 'submit')
+    .map(({ name, value }) => ({ [name]: value }))
+}
+
+function getPayload(formElem) {
+  return JSON.stringify(getFormData(formElem))
+}
+
+function handleError() {
+  // eslint-disable-next-line no-alert
+  alert(
+    'Senden der Nachricht fehlgeschlagen.\nBitte versuchen Sie es später noch einmal oder kontaktieren Sie mich unter thieme.garten@outlook.com.'
+  )
+}
+
+function handleSuccess(formElem) {
+  formElem.reset()
+}
+
+function handleSubmit(event) {
+  event.preventDefault()
+
+  const formElem = event.currentTarget
+  const payload = getPayload(formElem)
+
+  const xhr = new XMLHttpRequest()
+  xhr.open('POST', `.netlify/functions/sendMessage`)
+  xhr.send(payload)
+  xhr.addEventListener('load', () => {
+    switch (xhr.status) {
+      case 200:
+        handleSuccess(formElem)
+        break
+      default:
+        handleError()
+        break
+    }
+  })
+
+  xhr.addEventListener('error', () => {
+    handleError()
+  })
+}
 
 export default ContactForm
