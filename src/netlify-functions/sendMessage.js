7af6c842b31d5f4049adf782fcbb501c4266c8c0
Xaver Fleer <xaver.fleer@gmail.com>
2020-10-30 09:07:28 +0100

    Implement emails

--- src/netlify-functions/sendMessage.js
+++ src/netlify-functions/sendMessage.js
@@ -4,21 +4,20 @@ const responding = require('./private/responding')
 
 exports.handler = function absenden(event, context, callback) {
   logging.logStart('Start sendMessage')
-  // const respond = responding.responseHandlers(callback);
+  const respond = responding.responseHandlers(callback)
 
-  // logging.log(`event.body: ${event.body}`);
+  logging.log(`event.body: ${event.body}`)
 
-  // const msg = {
-  //   to: "xaver.fleer+soko-message@gmail.com",
-  //   subject: "Nachricht von Website gesendet",
-  //   text: event.body,
-  // };
-  // logging.log("sending email");
+  const msg = {
+    to: 'xaver.fleer+thieme-gartenbau@gmail.com',
+    subject: 'Nachricht von Website gesendet',
+    text: event.body,
+  }
+  logging.log('sending email')
 
-  // mailing
-  //   .sendEmail(msg)
-  //   .then(() => "success")
-  //   .catch(logging.logAndReject)
-  //   .then(respond.success, respond.failed);
-  responding.success('ok')
+  mailing
+    .sendEmail(msg)
+    .then(() => 'success')
+    .catch(logging.logAndReject)
+    .then(respond.success, respond.failed)
 }
