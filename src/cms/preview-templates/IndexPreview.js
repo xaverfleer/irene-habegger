d7fc4f10377381d0d9240fd7e268f3a596b34491
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-08 07:53:33 +0100

    Fix preview of images

--- src/cms/preview-templates/IndexPreview.js
+++ src/cms/preview-templates/IndexPreview.js
@@ -1,7 +1,27 @@
 import HomeIndex from '../../pages/index'
 
+const absLink = (relLink) => relLink.split('../../static')[1]
+
 export default ({ entry }) => {
   const data = entry.getIn(['data']).toJS()
+  data.bgImage = {
+    childImageSharp: { fluid: { src: absLink(data.bgImage) } },
+  }
+  data.meImage = {
+    childImageSharp: {
+      fixed: { src: absLink(data.meImage), width: 200, height: 200 },
+    },
+  }
+
+  data.works = data.works.map((w) => ({
+    ...w,
+    fluidLarge: {
+      childImageSharp: { fluid: { aspectRatio: 1.33, src: absLink(w.img) } },
+    },
+    fluidThumb: {
+      childImageSharp: { fluid: { aspectRatio: 1.33, src: absLink(w.img) } },
+    },
+  }))
 
   return HomeIndex({
     data: {
