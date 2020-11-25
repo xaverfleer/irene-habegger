55c0ab1ff1ae2b1dd57aeb568bfddbdbed08836b
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-25 17:59:59 +0100

    Add appData.js

---
+++ src/appData.js
@@ -0,0 +1,29 @@
+import bowlAndStone from '../static/img/black-02-Schale-und-Stein.jpg'
+import bark from '../static/img/black-03-Rinde.jpg'
+import unnamed from '../static/img/black-04-No-name.jpg'
+
+export default {
+  blackSeries: [
+    {
+      title: 'Bowl and stone',
+      src: bowlAndStone,
+      technique: 'oil on canvas',
+      size: '150 x 200 cm',
+      year: '2017',
+    },
+    {
+      title: 'Bark',
+      src: bark,
+      technique: 'oil on canvas',
+      size: '150 x 200 cm',
+      year: '2016',
+    },
+    {
+      title: 'Unnamed',
+      src: unnamed,
+      technique: 'oil on canvas',
+      size: '150 x 200 cm',
+      year: '2017',
+    },
+  ],
+}
--- src/pages/black-series.js
+++ src/pages/black-series.js
@@ -3,36 +3,10 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/Layout'
 
-import bowlAndStone from '../../static/img/black-02-Schale-und-Stein.jpg'
-import bark from '../../static/img/black-03-Rinde.jpg'
-import unnamed from '../../static/img/black-04-No-name.jpg'
+import appData from '../appData'
 
 const HomeIndex = () => {
   const siteDescription = 'Irene Habegger | Paintings'
-
-  const imgs = [
-    {
-      title: 'Bowl and stone',
-      src: bowlAndStone,
-      technique: 'oil on canvas',
-      size: '150 x 200 cm',
-      year: '2017',
-    },
-    {
-      title: 'Bark',
-      src: bark,
-      technique: 'oil on canvas',
-      size: '150 x 200 cm',
-      year: '2016',
-    },
-    {
-      title: 'Unnamed',
-      src: unnamed,
-      technique: 'oil on canvas',
-      size: '150 x 200 cm',
-      year: '2017',
-    },
-  ]
 
   function ImgContainer({ props }) {
     return (
@@ -54,7 +28,7 @@ const HomeIndex = () => {
         <title>irene habegger / black series</title>
         <meta name="description" content={siteDescription} />
       </Helmet>
-      {imgs.map((img) => (
+      {appData.blackSeries.map((img) => (
         <ImgContainer props={img} />
       ))}
     </Layout>
