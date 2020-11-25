21c2d91b3aef2e4b45dfc3ba225aaabb82af07c3
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-25 17:55:16 +0100

    cleanup

--- src/pages/black-series.js
+++ src/pages/black-series.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout'
 
 import bowlAndStone from '../../static/img/black-02-Schale-und-Stein.jpg'
 import bark from '../../static/img/black-03-Rinde.jpg'
+import unnamed from '../../static/img/black-04-No-name.jpg'
 
 const HomeIndex = () => {
   const siteDescription = 'Irene Habegger | Paintings'
@@ -53,37 +54,9 @@ const HomeIndex = () => {
         <title>irene habegger / black series</title>
         <meta name="description" content={siteDescription} />
       </Helmet>
-      <div className="thumbs">
-        {imgs.map((img) => (
-          <ImgContainer props={img} />
-        ))}
-        <div className="img-container">
-          <img
-            alt="Foto"
-            className="img-container__img"
-            src="/img/black-03-Rinde.jpg"
-          />
-          <div className="img-container__description">
-            <div className="img-container__title">Wall 1</div>
-            <div>acrylic, oil and coal on canvas</div>
-            <div>155 x 100 cm</div>
-            <div>2017</div>
-          </div>
-        </div>
-        <div className="img-container">
-          <img
-            alt="Foto"
-            className="img-container__img"
-            src="/img/black-04-No-name.jpg"
-          />
-          <div className="img-container__description">
-            <div className="img-container__title">Wall 1</div>
-            <div>acrylic, oil and coal on canvas</div>
-            <div>155 x 100 cm</div>
-            <div>2017</div>
-          </div>
-        </div>
-      </div>
+      {imgs.map((img) => (
+        <ImgContainer props={img} />
+      ))}
     </Layout>
   )
 }
