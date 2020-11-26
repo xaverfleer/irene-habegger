9aada022e51f26a2d3c8646f5d1f92e0708b85f1
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-26 18:41:12 +0100

    Fix order of appData

--- src/appData.js
+++ src/appData.js
@@ -47,32 +47,6 @@ const appData = {
       },
     ],
   },
-  blackSeries: {
-    title: 'Irene Habegger | Black series',
-    imgs: [
-      {
-        title: 'Bowl and stone',
-        src: bowlAndStone,
-        technique: 'oil on canvas',
-        size: '150 x 200 cm',
-        year: '2017',
-      },
-      {
-        title: 'Bark',
-        src: bark,
-        technique: 'oil on canvas',
-        size: '150 x 200 cm',
-        year: '2016',
-      },
-      {
-        title: 'Unnamed',
-        src: unnamed,
-        technique: 'oil on canvas',
-        size: '150 x 200 cm',
-        year: '2017',
-      },
-    ],
-  },
   portraits: {
     title: 'Irene Habegger | Selection series',
     imgs: [
@@ -123,6 +97,44 @@ const appData = {
         src: portrait10Xaver,
         technique: 'oil on canvas',
         size: '40 x 30 cm',
+        year: '2017',
+      },
+    ],
+  },
+  hedges: {
+    title: 'Irene Habegger | Hedges',
+    imgs: [
+      {
+        title: 'Bowl and stone',
+        src: bowlAndStone,
+        technique: 'oil on canvas',
+        size: '150 x 200 cm',
+        year: '2017',
+      },
+    ],
+  },
+  blackSeries: {
+    title: 'Irene Habegger | Black series',
+    imgs: [
+      {
+        title: 'Bowl and stone',
+        src: bowlAndStone,
+        technique: 'oil on canvas',
+        size: '150 x 200 cm',
+        year: '2017',
+      },
+      {
+        title: 'Bark',
+        src: bark,
+        technique: 'oil on canvas',
+        size: '150 x 200 cm',
+        year: '2016',
+      },
+      {
+        title: 'Unnamed',
+        src: unnamed,
+        technique: 'oil on canvas',
+        size: '150 x 200 cm',
         year: '2017',
       },
     ],
