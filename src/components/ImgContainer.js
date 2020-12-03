3faa0c6f294493732f722c2074947b872876c3be
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-03 12:18:47 +0100

    Image description on one line

--- src/components/ImgContainer.js
+++ src/components/ImgContainer.js
@@ -6,10 +6,10 @@ const ImgContainer = ({ props }) => (
   <div className="img-container">
     <img alt={props.title} className="img-container__img" src={props.src} />
     <div className="img-container__description">
-      <div className="img-container__title">{props.title}</div>
-      <div>{props.technique}</div>
-      <div>{props.size}</div>
-      <div>{props.year}</div>
+      <span className="img-container__title">{props.title}, </span>
+      <span>
+        {props.technique}, {props.size}, {props.year}
+      </span>
     </div>
   </div>
 )
