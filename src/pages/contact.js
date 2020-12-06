dc1d34e0d504f5b191cf99cd74692360c4493383
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-06 18:20:19 +0100

    Add first form field

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,42 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/Layout'
 
-const HomeIndex = () => {
+const ContactFormEntry = ({ props }) => {
+  function handleInputChange(e) {
+    const inputElement = e.target
+    const entryElement = e.target.parentElement
+    const hasInput = 0 < inputElement.value.length
+
+    hasInput
+      ? entryElement.classList.add('contact-form__entry--non-empty')
+      : entryElement.classList.remove('contact-form__entry--non-empty')
+  }
+
+  const inputId = `contact-form-${props.name}`
+
+  return (
+    <div className="contact-form__entry">
+      {props.type === 'text' && (
+        <input
+          className="contact-form__input"
+          id={inputId}
+          name={props.name}
+          onInput={handleInputChange}
+          required={props.required}
+          title={props.label}
+          type={props.type}
+        />
+      )}
+      <label className="contact-form__label" for={inputId}>
+        {props.label}
+        <span class="contact-form__required-indicator">
+          {props.required && '*'}
+        </span>
+      </label>
+    </div>
+  )
+}
+
   const siteDescription = 'Irene Habegger | Contact'
 
   return (
@@ -13,6 +48,17 @@ const HomeIndex = () => {
         <meta name="description" content={siteDescription} />
       </Helmet>
       I look forward to your message.
+      <form className="contact-form">
+        <ContactFormEntry
+          className="contact-form__entry"
+          props={{
+            label: 'Name',
+            name: 'name',
+            type: 'text',
+            required: true,
+          }}
+        />
+      </form>
     </Layout>
   )
 }
