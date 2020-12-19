41b3ccffa50bf463260b343edd019b4e821bd0f5
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-19 09:51:16 +0100

    Add in the morning light

---
+++ src/pages/in-the-morning-light.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import ImgContainer from '../components/ImgContainer'
+import Layout from '../components/Layout'
+
+import { inTheMorningLight as imgData } from '../appData'
+
+const page = () => (
+  <Layout>
+    <Helmet>
+      <title>{imgData.title}</title>
+      <meta name="description" content={imgData.title} />
+    </Helmet>
+    <div className="gallery">
+      {imgData.imgs.map((img) => (
+        <ImgContainer props={img} />
+      ))}
+    </div>
+  </Layout>
+)
+
+export default page
