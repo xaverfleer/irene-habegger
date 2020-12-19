4dd8d3ee6d167a3d515bec5dd08340e253d20323
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-19 11:24:45 +0100

    Add Moonlight painting

--- src/appData.js
+++ src/appData.js
@@ -10,7 +10,7 @@ import object01a from '../static/img/object-01a.jpg'
 import object02a from '../static/img/object-08.jpg'
 import object03a from '../static/img/object-03a.jpg'
 import object04a from '../static/img/object-04a.jpg'
-import object05a from '../static/img/object-01.jpg'
+import object05a from '../static/img/object-05a.jpg'
 import object05 from '../static/img/object-05.jpg'
 import object06 from '../static/img/object-06.jpg'
 import object12 from '../static/img/object-12.jpg'
@@ -183,11 +183,10 @@ const appData = {
         size: '30 x 24 cm',
       },
       {
-        title: 'Blue',
+        title: 'Moonlight',
         src: object05a,
         technique: 'oil on canvas',
-        size: '50 x 50 cm',
-        year: '2016',
+        size: '40 x 40 cm',
       },
       {
         title: 'Object on the Wall',
