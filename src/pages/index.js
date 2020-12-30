ee344e01f98abcd02f1635bf2502caf3f5d8a7dd
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-30 23:57:27 +0100

    Import graphql from 'gatsby'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
 
 import ImgContainer from '../components/ImgContainer'
 import Layout from '../components/Layout'
