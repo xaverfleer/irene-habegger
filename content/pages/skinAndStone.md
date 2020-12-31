6f27f722b1f87a8f4106339728192c11b5d0e8bb
Xaver Fleer <xaver.fleer@gmail.com>
2020-12-31 23:14:26 +0100

    Add skinAndStone to NetlifyCMS

---
+++ content/pages/skinAndStone.md
@@ -0,0 +1,16 @@
+---
+templateKey: skinAndStone
+htmlDescription: Skin and stone series (2016 - 2017)
+htmlTitle: Irene Habegger | Skin and stone
+paintings:
+  - img: ../../static/img/black-02-Schale-und-Stein.jpg
+    title: Skin and stone
+    technique: oil on canvas
+    dimensions: 150 x 200 cm
+    year: '2017'
+  - img: ../../static/img/black-03-Rinde.jpg
+    title: Bark
+    technique: oil on canvas
+    dimensions: 150 x 200 cm
+    year: '2016'
+---
--- src/pages/skin-and-stone.js
+++ src/pages/skin-and-stone.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
 
-import ImgContainer from '../components/ImgContainer'
-import Layout from '../components/Layout'
+import Gallery from '../components/Gallery'
 
-import { skinAndStone as imgData } from '../appData'
+export default Gallery
 
-export default () => (
-  <Layout>
-    <Helmet>
-      <title>{imgData.title}</title>
-      <meta name="description" content={imgData.description || imgData.title} />
-    </Helmet>
-    <div className="gallery">
-      {imgData.imgs.map((img) => (
-        <ImgContainer props={img} />
-      ))}
-    </div>
-  </Layout>
-)
+export const pageQuery = graphql`
+  {
+    markdownRemark(frontmatter: { templateKey: { eq: "skinAndStone" } }) {
+      frontmatter {
+        htmlDescription
+        htmlTitle
+        paintings {
+          dimensions
+          img {
+            publicURL
+          }
+          technique
+          title
+          year
+        }
+      }
+    }
+  }
+`
