4cc52cb59cb22d1ac3468fafde5278584902da14
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-01 17:23:01 +0100

    Add bio NetlifyCMS

---
+++ content/pages/bio.md
@@ -0,0 +1,24 @@
+---
+templateKey: bio
+htmlDescription: My biography (little) depth.
+htmlTitle: Irene Habegger | Bio
+paintings:
+img: ../../static/img/yo_.jpg
+---
+
+## Bio
+
+Irene Habegger is an Argentinean artist who was born in Buenos Aires in 1989. There she started to study drawing and painting at the painting school of Guillermo Roux. In 2014 she moved to Switzerland, where she obtained a degree in Fine Arts from the University of the Arts Bern. She completed her studies in Italy, where she did a master's degree in painting at the Accademia di Belle Arti di Brera in Milan. She currently lives between Italy and Switzerland.
+
+## Exhibitions
+
+- 2020 Kunsthaus Steffisburg, Cantonale Berne Jura, CH
+- 2017 Kunsthaus Langenthal, Fifteen, Diploma exhibition, CH
+- 2016 Musée jurassien des Arts, Moutier, Cantonale Berne Jura, CH
+- 2014 Centro cultural El Faro. Pinturas (solo exhibition), Buenos Aires, AR
+- 2013 VII Salon of Contemporary Art of San Isidro, Buenos Aires, AR
+
+## Prizes
+
+- 2014 Second prize 33 th Ward School Plein Air Painting competition, Buenos Aires, AR
+- 2011 First prize LIV Buenos Aires Annual Salon of Plein Air Painting, Buenos Aires, AR
--- src/pages/bio.js
+++ src/pages/bio.js
@@ -4,59 +4,45 @@ import Helmet from 'react-helmet'
 import ImgContainer from '../components/ImgContainer'
 import Layout from '../components/Layout'
 
-import yo from '../../static/img/yo_.jpg'
-
-export default () => {
-  const siteTitle = 'Irene Habegger | Bio'
-  const siteDescription = 'My biography (little) depth.'
+export default ({ data }) => {
+  const { htmlDescription, htmlTitle, img } = data.markdownRemark.frontmatter
 
   return (
     <Layout>
       <Helmet>
-        <title>{siteTitle}</title>
-        <meta name="description" content={siteDescription} />
+        <title>{htmlTitle}</title>
+        <meta name="description" content={htmlDescription} />
       </Helmet>
       <div class="gallery gallery-bio">
         <ImgContainer
-          props={{ name: 'bio', noCaption: true, src: yo, title: 'Irene' }}
+          props={{
+            name: 'bio',
+            noCaption: true,
+            src: img.publicURL,
+            title: 'Irene',
+          }}
         />
       </div>
-      <h2>Bio</h2>
-      <p>
-        Irene Habegger is an Argentinean artist who was born in Buenos Aires in
-        1989. There she started to study drawing and painting at the painting
-        school of Guillermo Roux. In 2014 she moved to Switzerland, where she
-        obtained a degree in Fine Arts from the University of the Arts Bern. She
-        completed her studies in Italy, where she did a master's degree in
-        painting at the Accademia di Belle Arti di Brera in Milan. She currently
-        lives between Italy and Switzerland.
-      </p>
-      <h2>Exhibitions</h2>
-      <ul>
-        <li>2020 Kunsthaus Steffisburg, Cantonale Berne Jura, CH</li>
-        <li>2017 Kunsthaus Langenthal, Fifteen, Diploma exhibition, CH</li>
-        <li>
-          2016 Musée jurassien des Arts, Moutier, Cantonale Berne Jura, CH
-        </li>
-        <li>
-          2014 Centro cultural El Faro. Pinturas (solo exhibition), Buenos
-          Aires, AR
-        </li>
-        <li>
-          2013 VII Salon of Contemporary Art of San Isidro, Buenos Aires, AR
-        </li>
-      </ul>
-      <h2>Prizes</h2>
-      <ul>
-        <li>
-          2014 Second prize 33 th Ward School Plein Air Painting competition,
-          Buenos Aires, AR
-        </li>
-        <li>
-          2011 First prize LIV Buenos Aires Annual Salon of Plein Air Painting,
-          Buenos Aires, AR
-        </li>
-      </ul>
+      <article
+        dangerouslySetInnerHTML={{
+          __html: data.markdownRemark.html,
+        }}
+      ></article>
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  {
+    markdownRemark(frontmatter: { templateKey: { eq: "bio" } }) {
+      frontmatter {
+        htmlDescription
+        htmlTitle
+        img {
+          publicURL
+        }
+      }
+      html
+    }
+  }
+`
